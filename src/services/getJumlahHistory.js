f4e1615e13ea3ff70c1904c330b4653fc94f80bb
ilhanmanzis <ilhanmanzis1207@gmail.com>
2024-12-26 23:51:04 +0700

    memperbaiki bug save data history scan ke firestore

--- src/services/getJumlahHistory.js
+++ src/services/getJumlahHistory.js
@@ -3,7 +3,9 @@ import db from "../config/db.js"
 const getJumlahHistory = async(id)=>{
     const historyCollection = db.collection('users').doc(id).collection('history');
     try {
-        const queryHistory = await getDocs(historyCollection);
+        const queryHistory = await historyCollection.get(historyCollection);
+        console.log(queryHistory);
+        console.log(queryHistory.size);
         return queryHistory.empty ? 0 : queryHistory.size;  // Menambahkan pengecekan jika subcollection kosong
     } catch (error) {
         console.error("Error getting documents: ", error);
